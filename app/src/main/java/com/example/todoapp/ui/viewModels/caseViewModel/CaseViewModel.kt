6114e6757df76dd59c6a56f8ffe3fa5a1c8cf3db
kirill-ka123 <regm0033@mail.ru>
2022-08-07 16:16:59 +0300

    Сделал переиспользуемый scope в caseViewModel.

--- app/src/main/java/com/example/todoapp/ui/viewModels/caseViewModel/CaseViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/viewModels/caseViewModel/CaseViewModel.kt
@@ -5,15 +5,18 @@ import com.example.todoapp.models.TodoItem
 import com.example.todoapp.repository.TodoItemsRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 
 class CaseViewModel(private val todoItemsRepository: TodoItemsRepository) : ViewModel() {
 
-    fun saveTodoItem(todoItem: TodoItem) = CoroutineScope(Dispatchers.IO).launch {
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    fun saveTodoItem(todoItem: TodoItem) = scope.launch {
         todoItemsRepository.upsertTodoItem(todoItem)
     }
 
-    fun deleteTodoItem(todoItem: TodoItem) = CoroutineScope(Dispatchers.IO).launch {
+    fun deleteTodoItem(todoItem: TodoItem) = scope.launch {
         todoItemsRepository.deleteTodoItem(todoItem)
     }
 }
